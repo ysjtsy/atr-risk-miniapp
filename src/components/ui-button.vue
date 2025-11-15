48801ab6012011b84cae57893cd19ac9e2575bd0
qszOrangebro <129163154+ysjtsy@users.noreply.github.com>
2025-11-15 20:13:21 +0800

    Port core page logic to Vue3

---
+++ src/components/ui-button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    class="btn"
+    :loading="props.loading"
+    :disabled="props.disabled"
+    @tap="handleTap"
+  >
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['tap'])
+
+const handleTap = () => {
+  emit('tap')
+}
+</script>
+
+<style scoped lang="scss">
+.btn {
+  width: 100%;
+  border: none;
+}
+</style>
