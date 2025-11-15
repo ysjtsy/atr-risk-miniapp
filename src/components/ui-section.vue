ae7c222e83790d7489dd6b9f8696c386e69f090f
qszOrangebro <129163154+ysjtsy@users.noreply.github.com>
2025-11-15 20:13:49 +0800

    Port core page logic to Vue3

---
+++ src/components/ui-section.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="section">
+    <view class="label">{{ props.label }}</view>
+    <view v-if="props.subLabel" class="sub-label">{{ props.subLabel }}</view>
+    <slot />
+  </view>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  subLabel: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<style scoped>
+.section {
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/config/trading.js
@@ -0,0 +1,2 @@
+export const SYMBOL_OPTIONS = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT']
+export const INTERVAL_OPTIONS = ['15m', '30m', '1h', '4h', '1d']
